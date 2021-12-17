b6e97b970c6e9a42069ec9eff10116ca306de152
Samuel Sandgren <samuel.b.sandgren@gmail.com>
2021-12-17 06:12:13 +0100

    Add day 6

---
+++ 6/lanternfish.py
@@ -0,0 +1,15 @@
+class lanternfish:
+    def __init__(self, cycle) -> None:
+        self.cycle = cycle
+
+    def spawn(self):
+        newFish = lanternfish(8)
+        return newFish
+
+    def dayChange(self):
+        self.cycle -= 1
+        if self.cycle < 0:
+            self.cycle = 6
+            return self.spawn()
+        else:
+            return False
---
+++ 6/puzzle1.py
@@ -0,0 +1,33 @@
+import lanternfish
+
+dataFile = open('./input', 'r')
+data = dataFile.read().split(',')
+dataFile.close()
+
+fishSchool = []
+for fish in data:
+    fishSchool.append(lanternfish.lanternfish(int(fish)))
+
+outputFile = open('./output', 'w')
+outputFile.write('')
+outputFile.close()
+
+for day in range(80):
+    for fish in fishSchool:
+        newFish = fish.dayChange()
+        if newFish:
+            fishSchool.append(newFish)
+    
+    population = []
+    for fish in fishSchool:
+        population.append(str(fish.cycle))
+
+    report = f"Day {day}: "
+    report = report + ",".join(population) + '\n'
+    
+    outputFile = open('./output', 'a')
+    outputFile.write(report)
+    outputFile.close()
+
+populationSize = len(fishSchool)
+print(f"Population: {populationSize}")
